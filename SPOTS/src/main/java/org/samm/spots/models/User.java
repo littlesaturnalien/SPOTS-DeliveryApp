14e4ce10220b96e0fb23c1be9f2c1245fcb01bf9
vexfairyprincess <samora@uamv.edu.ni>
2025-06-12 17:47:28 -0600

    attributes added in class user

--- SPOTS/src/main/java/org/samm/spots/models/User.java
+++ SPOTS/src/main/java/org/samm/spots/models/User.java
@@ -3,12 +3,22 @@ package org.samm.spots.models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "Usuario")
+@Getter @Setter
 public class User {
     @Id
     private String email;
 
     private String password;
+
+    private String adress;
+
+    private String phone;
+
+    private String role;
+
 }

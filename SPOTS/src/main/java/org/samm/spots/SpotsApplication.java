47a7edaa7b8bc30c64cf5634bc5aedb0094c4aaf
vexfairyprincess <samora@uamv.edu.ni>
2025-06-12 09:55:23 -0600

    springboot project initialized

---
+++ SPOTS/src/main/java/org/samm/spots/SpotsApplication.java
@@ -0,0 +1,13 @@
+package org.samm.spots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotsApplication.class, args);
+    }
+
+}
---
+++ SPOTS/src/test/java/org/samm/spots/SpotsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.samm.spots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

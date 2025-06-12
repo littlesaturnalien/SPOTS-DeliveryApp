51056cbfed1bcce4f72c6771143f9676e751c4ec
littlesaturnalien <kmryfonseca@uamv.edu.ni>
2025-06-12 17:48:40 -0600

    Jakarta Validation Dependency added

--- SPOTS/src/main/java/org/samm/spots/models/User.java
+++ SPOTS/src/main/java/org/samm/spots/models/User.java
@@ -1,14 +1,22 @@
 package org.samm.spots.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotEmpty;
 
 @Entity
-@Table(name = "Usuario")
+@Table(name = "Tb_User")
 public class User {
     @Id
+    @Email(message = "Email is not valid", regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
+    @NotEmpty(message = "Email cannot be empty")
     private String email;
 
+    @Column(name = "contraseña")
+    @Max
     private String password;
 }
